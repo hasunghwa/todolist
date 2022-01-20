875f6eae5cdd6309fa92ad5ca474e07c89369604
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-01-20 19:16:23 +0900

    upload todolist

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { atom, useSetRecoilState } from "recoil";
+import { Categories, categoryState, IToDo, toDoState } from "../atoms";
+
+function ToDo({text, category, id}: IToDo){
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const { currentTarget: {name}, } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any};
+      return [
+        ...oldToDos.slice(0, targetIndex), 
+        newToDo, 
+        ...oldToDos.slice(targetIndex+1),
+      ];
+    })
+  }
+  
+  const onDelete = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      let _oldToDos = Array.from(oldToDos);
+      _oldToDos.splice(targetIndex, 1);
+      return _oldToDos;
+    })
+  }
+
+  return  (
+    <li>
+      <span>{text}</span>
+      {category != Categories.DOING && <button name={Categories.DOING+""} onClick={onClick}>Doing</button>}
+      {category != Categories.TO_DO && <button name={Categories.TO_DO+""} onClick={onClick}>To Do</button>}
+      {category != Categories.DONE && <button name={Categories.DONE+""} onClick={onClick}>Done</button>}
+      <button onClick={onDelete}>Delete</button>
+    </li>
+  );
+}
+
+export default ToDo;
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {darkTheme} from "./theme";
+import {ThemeProvider} from 'styled-components';
+import {RecoilRoot} from 'recoil';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App /> 
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
